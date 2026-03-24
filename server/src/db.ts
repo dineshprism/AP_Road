56c52a6cda2f973768d950dcdfa9ccb527e152e1
dineshjnld <dineshjnld22@gmail.com>
2026-03-24 18:07:38 +0530

    ok

--- server/src/db.ts
+++ server/src/db.ts
@@ -3,9 +3,23 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+function getPoolConfig(): pg.PoolConfig {
+  if (process.env.DATABASE_URL) {
+    return {
+      connectionString: process.env.DATABASE_URL,
+    };
+  }
+
+  return {
+    host: process.env.PGHOST || "localhost",
+    port: parseInt(process.env.PGPORT || "5432", 10),
+    user: process.env.PGUSER || "postgres",
+    password: process.env.PGPASSWORD || "",
+    database: process.env.PGDATABASE || "road_accident_db",
+  };
+}
+
+const pool = new pg.Pool(getPoolConfig());
 
 pool.on("error", (err) => {
   console.error("Unexpected error on idle client", err);
